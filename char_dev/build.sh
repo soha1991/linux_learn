0ee3421cf11130e499a5ab557d58ce16eb2534e2
Hang Song <hang.song.cn@windriver.com>
2025-04-25 10:09:11 +0800

    Add macOS CROSS_COMPILE

--- char_dev/build.sh
+++ char_dev/build.sh
@@ -1,1 +1,5 @@
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+if [[ "$(uname)" == "Linux" ]]; then
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+elif [[ "$(uname)" == "Darwin" ]]; then
+    make ARCH=arm64 CROSS_COMPILE=aarch64-rpi3-linux-gnu-
+fi
